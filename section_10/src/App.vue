355fd140928ea4741a01ac5520ad162e232d46ca
Javier Cabezas <jcleyton@gmail.com>
2017-04-19 21:25:24 -0300

    Using components to loop the quotes

--- section_10/src/App.vue
+++ section_10/src/App.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="container">
-    
+        <quote v-for="q in quotes" :quote="q">
+        </quote>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
+
     export default {
-        
+        data: function (){
+            return {
+                quotes: [
+                    'Starting quote',
+                    'Another quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components:{
+            'quote': Quote
+        }
     }
 </script>
 
---
+++ section_10/src/components/Quote.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="quote_block">
+        <p> {{ quote }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quote']
+    }
+</script>
+
+<style>
+
+</style>
