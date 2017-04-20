e3f596ead727b59a6f3b072f3cb21dcbc2a5637b
Javier Cabezas <jcleyton@gmail.com>
2017-04-19 23:23:16 -0300

    Using computed properties to render the progress bar

--- section_10/src/components/QuoteSlider.vue
+++ section_10/src/components/QuoteSlider.vue
@@ -1,12 +1,22 @@
 <template>
-   <div class="row slider">
-       {{ quotes_num }} / {{ max_quotes }}
-   </div>
+
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" :aria-valuenow="quotes_num"
+             aria-valuemin="0" :aria-valuemax="max_quotes" :style="'width:'+ percentage +'%'">
+            {{quotes_num}} / {{max_quotes}} ({{percentage}}%)
+        </div>
+    </div>
+
 </template>
 
 <script>
     export default {
-        props: ['quotes_num', 'max_quotes']
+        props: ['quotes_num', 'max_quotes'],
+        computed: {
+            percentage: function () {
+                return 100 * this.quotes_num / this.max_quotes;
+            }
+        }
     }
 </script>
 
