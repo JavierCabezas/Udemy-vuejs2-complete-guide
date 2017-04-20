4e6d96a4dd05b25883bb05db8f8ea79b757aafbc
Javier Cabezas <jcleyton@gmail.com>
2017-04-19 21:58:50 -0300

    Components for the slider

--- section_10/src/App.vue
+++ section_10/src/App.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="container">
-        <quote v-for="q in quotes" :quote="q">
-        </quote>
+        <quote-slider :quotes_num="quotes.length" :max_quotes="maxQuotes"> </quote-slider>
+        <quote v-for="q in quotes" :quote="q"> </quote>
     </div>
 </template>
 
 <script>
     import Quote from './components/Quote.vue';
+    import Quoteslider from './components/QuoteSlider.vue';
 
     export default {
         data: function (){
             return {
                 quotes: [
                     'Starting quote',
-                    'Another quote'
+                    'Another quote',
                 ],
                 maxQuotes: 10
             }
         },
         components:{
-            'quote': Quote
+            'quote': Quote,
+            'quoteSlider': Quoteslider
         }
     }
 </script>
---
+++ section_10/src/components/QuoteSlider.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="slider">
+       {{ quotes_num }} / {{ max_quotes }}
+   </div>
+</template>
+
+<script>
+    export default {
+        props: ['quotes_num', 'max_quotes']
+    }
+</script>
+
+<style>
+
+</style>
