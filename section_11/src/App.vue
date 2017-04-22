cc670aaf18e880edc60cbd5ad4e7622da61f24b1
Javier Cabezas <jcleyton@gmail.com>
2017-04-22 14:29:37 -0300

    Section 11 ex 1

--- section_11/src/App.vue
+++ section_11/src/App.vue
@@ -3,12 +3,38 @@
         <form>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+
+                    <div class="form">
+                        <h2>Signup form</h2>
+                        <!-- Exercise 1 -->
+                        <!-- Create a Signup Form where you retrieve the following Information -->
+                        <!-- Full Name (First Name + Last Name) -->
+                        <!-- Mail -->
+                        <!-- Password -->
+                        <!-- Store Data? Yes/No -->
+
+                        <div class="form-group">
+                            <label for="input_full_name">Full name</label>
+                            <input type="text" class="form-control" v-model="input_full_name" id="input_full_name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="input_email">Full name</label>
+                            <input type="email"  class="form-control" v-model="input_email" id="input_email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="input_password"> Password</label>
+                            <input type="password"  class="form-control" v-model="input_password" id="input_password">
+                        </div>
+
+                        <div class="form-group">
+                            <div class="checkbox">
+                                <label><input type="checkbox" v-model="input_store_in_database"> Store in database? </label>
+                            </div>
+                        </div>
+                    </div>
+
 
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
@@ -28,10 +54,10 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{input_full_name}} </p>
+                        <p>Mail: {{input_email}}</p>
+                        <p>Password:{{input_password}} </p>
+                        <p>Store in Database?:{{input_store_in_database}} </p>
                     </div>
                 </div>
             </div>
@@ -41,6 +67,16 @@
 
 <script>
     export default {
+        data: function(){
+            return {
+                input_full_name: '',
+                input_email: '',
+                input_password: '',
+                input_store_in_database: true
+            }
+        },
+        components:{
+        }
     }
 </script>
 
