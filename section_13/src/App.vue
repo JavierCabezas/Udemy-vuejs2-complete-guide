cf2f4a0aa43b8d2494be3fe66f801317ca1e2f16
Javier Cabezas <jcleyton@gmail.com>
2017-04-24 21:31:03 -0300

    Use of computed properties

--- section_13/src/App.vue
+++ section_13/src/App.vue
@@ -5,18 +5,22 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
-                <p> {{ "reverse" | capitalize }} </p>
+                <p> {{ reverseMe | capitalize }} </p>
 
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                <p> {{ "counter" | add-character-count }}</p>
+                <p> {{ countMe | add-character-count }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <p> {{reversed}}</p>
 
+                <p> {{counted}}</p>
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+
+                
             </div>
         </div>
     </div>
@@ -24,9 +28,23 @@
 
 <script>
     export default {
+        data: function (){
+            return {
+                reverseMe: 'reverse',
+                countMe: 'Counter'
+            }
+        },
         filters: {
             capitalize: function (value) {
                return value.split("").reverse().join("");
+            }
+        },
+        computed: {
+            reversed() {
+                return this.reverseMe.split("").reverse().join("");
+            },
+            counted() {
+                return this.countMe + ' (' + this.countMe + ')';
             }
         }
     }
